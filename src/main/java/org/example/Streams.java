acf90da72120de1c2cff8b9210f65bbf9f7c83d0
Sameer <sameer997sa@gmail.com>
2024-10-30 11:41:57 +0530

    Third Commit

--- src/main/java/org/example/Streams.java
+++ src/main/java/org/example/Streams.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Streams {
@@ -15,9 +18,11 @@ public class Streams {
 //         stream.forEach((emp)->System.out.println(emp.getName()));
 //         List<Employee> emp=stream.filter(e->e.getName().startsWith("Ma") || e.getName().startsWith("Bill")).collect(Collectors.toList());
 //        Employee emp=stream.filter(e->e.getName().contains("B")).findFirst().orElse(null);
-        stream.peek(e->System.out.println(e.getSalary())).collect(Collectors.toList());
+//        stream.peek(e->System.out.println(e.getSalary())).collect(Collectors.toList());
 //         System.out.println(emp.getName());
 
-
+        List<String> list= Arrays.asList("999999999","987654321");
+        IntStream longStream=list.stream().mapToInt(Integer::parseInt);
+        longStream.forEach(System.out::println);
     }
 }
